fe9597e44dadb70c038251e7ddb68d84ee6081d1
techno-express <technoexpressnet@gmail.com>
2020-10-24 23:16:39 -0400

    revert merge, use my forked `node-wget-js`, and use `node-stream-zip` for unzip
    `wget-improved` has too many dependencies as do `unzipper`
    
    `node-wget-js` will be rewritten to not use deprecated `request` module

--- installer.js
+++ installer.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
+
 'use strict'
 
 var fs = require('fs');
 var path = require('path');
 
-const unarAppFile = (process.platform == "darwin") ? 'unar1.8.1.zip' : 'unar1.8.1_win.zip' ;
+const unarAppFile = (process.platform == "darwin") ? 'unar1.8.1.zip' : 'unar1.8.1_win.zip';
 const unarAppUrl = 'https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/theunarchiver/';
 
 const cwd = process.cwd();
@@ -13,41 +14,60 @@ const source = path.join(cwd, unarAppFile);
 
 if ((process.platform == "win32") || (process.platform == "darwin")) {
     getExtractUnar(url, source, cwd)
-    .then(function() {
-        fs.unlink(source, (err) => { if (err) console.error(err); });
-        if (process.platform != "win32") {
-            var chmod = ['unar', 'lsar'];
-            chmod.forEach(function(s) { fs.chmodSync(path.join(cwd,s), 755) });
-        }
-        console.log('Unar installed successful');
-    })
-    .catch(function (err) { console.log(err); });
+        .then(function () {
+            fs.unlink(source, (err) => {
+                if (err) console.error(err);
+            });
+            if (process.platform != "win32") {
+                var chmod = ['unar', 'lsar'];
+                chmod.forEach(function (s) {
+                    fs.chmodSync(path.join(cwd, s), 755)
+                });
+            }
+            console.log('Unar installed successful');
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
 } else {
-	const system_installer = require('system-installer').installer;
-	system_installer('unar')
-    .then(function() {
-        console.log('Unar installed successful');
-    })
-    .catch(function (err) { console.error(err); });
+    const system_installer = require('system-installer').installer;
+    system_installer('unar')
+        .then(function () {
+            console.log('Unar installed successful');
+        })
+        .catch(function (err) {
+            console.error(err);
+        });
 }
 
-function getExtractUnar(urlSource, fileSource, destination){
-    var node_wget = require("wget-improved");
-    var unzip = require('unzipper');
+function getExtractUnar(urlSource, fileSource, destination) {
+    var node_wget = require("node-wget-js");
+    var StreamZip = require('node-stream-zip');
     console.log('Downloading ' + urlSource);
-  return new Promise(function (resolve, reject) {
+    return new Promise(function (resolve, reject) {
+        node_wget({
+            url: urlSource,
+            dest: fileSource
+        }, function (err) {
+            if (err) {
+                return reject('Error downloading file: ' + err);
+            }
 
-    let download = node_wget.download(urlSource, fileSource, {});
-    download.on('error', function(err) {
-        console.error('Error downloading file: ' + err);
-        reject(err);
-    });
-    download.on('end', function(output) {
-        var unZipFile = unzip.Extract({ path: destination });
-        unZipFile.on('error', function(err) { reject(err); });
-        unZipFile.on('close', function() { resolve(); });
-        fs.createReadStream(fileSource).pipe(unZipFile);
-    });
+            var unzip = new StreamZip({
+                file: fileSource,
+                storeEntries: true
+            });
 
-  });
+            unzip.on('ready', () => {
+                unzip.extract(null, destination, (err, count) => {
+                    unzip.close();
+                    if (err) {
+                        return reject(err);
+                    }
+
+                    return resolve(count);
+                });
+            });
+        });
+    });
 }
